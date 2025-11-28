56700479de5c82cd3f73133e565de0d4fad3f969
BikaashMeher <bm244368@gmail.com>
2025-11-28 20:53:22 +0530

    first commit

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,66 @@
+// import React from 'react'
+// import { Outlet } from 'react-router-dom'
+// import NavBar from '../components/NavBar'
+// import { useSelector } from 'react-redux'
+// import Login from './Login'
+// import LoginLoading from '../components/loginLoading'
+
+// const Layout = () => {
+
+
+//   const { user, loading } = useSelector(state => state.auth)
+
+//   if (loading) {
+//     return <LoginLoading />
+//   }
+
+//   return (
+//     <div>
+//       {user ? (
+//         <div className='min-h-screen bg-gray-50'>
+//           <NavBar />
+//           <Outlet />
+
+//         </div>
+//       ) : <Login />}
+//     </div>
+//   )
+// }
+
+// export default Layout
+
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import NavBar from '../components/NavBar'
+import { useSelector } from 'react-redux'
+import Login from './Login'
+import LoginLoading from '../components/loginLoading'
+
+const Layout = () => {
+
+  const { user, loading } = useSelector(state => state.auth)
+
+  if (loading) {
+    return <LoginLoading />
+  }
+
+  return (
+    <div>
+      {user ? (
+        <div className='min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-slate-100 relative overflow-hidden'>
+
+          {/* Navbar */}
+          <NavBar />
+
+          {/* Main content with relative positioning */}
+          <div className='relative z-10'>
+            <Outlet />
+          </div>
+
+        </div>
+      ) : <Login />}
+    </div>
+  )
+}
+
+export default Layout
